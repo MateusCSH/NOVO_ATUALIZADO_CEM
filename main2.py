53a422cda1c4e261998e76b71c3d8a32ecb88117
MateusCSH <91564533+MateusCSH@users.noreply.github.com>
2024-07-15 16:17:29 -0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -17,6 +17,7 @@ st.set_page_config(page_title='Dashboard CEM')
 with open("styles3.css", 'r', encoding='utf-8') as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
+st.markdown('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">', unsafe_allow_html=True)
 
 
 
